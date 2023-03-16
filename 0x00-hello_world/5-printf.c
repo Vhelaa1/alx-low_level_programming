b837f20e0588579f38c8595b5b507536ca535876
HALA <vhelatique123@gmail.com>
2023-03-16 15:44:59 -0700

    again

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - main function
+ *
+ * Return: 0 when succesfull
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
