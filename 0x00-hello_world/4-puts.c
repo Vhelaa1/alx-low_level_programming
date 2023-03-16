7b8e112ee8ad33e496731dac424125367caabab9
HALA <vhelatique123@gmail.com>
2023-03-16 15:12:29 -0700

    hello c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+   puts("\"Programming is like building a multilingual puzzle);
+   return (0);
+}
