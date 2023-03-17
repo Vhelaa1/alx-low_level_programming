e3fded71bac8f8fab2ea8aebee6c82b73c52168c
HALA <vhelatique123@gmail.com>
2023-03-17 13:57:39 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the numbers 0 to 9, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char numb;
+
+	for (numb = 0; numb <= 9; numb++)
+	{
+		putchar(numb + '0');
+	}
+	putchar('\n');
+	return (0);
+}
+
