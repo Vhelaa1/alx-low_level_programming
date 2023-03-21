bac9ee5fd55a4295ad41d3d01497e89f9aaf1207
HALA <vhelatique123@gmail.com>
2023-03-21 15:09:59 -0700

    l

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absul of a number
+ * @n: print number
+ *
+ * Return: the absolu of the int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
