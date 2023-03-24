48a0ba623be0a6e9494442127208d206866ca778
HALA <vhelatique123@gmail.com>
2023-03-23 18:09:35 -0700

    h

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - print Fizz-Buzz t numbers of 3 and 5.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a;
+
+	a = 1;
+	printf("%d", a);
+	for (a = 2; a <= 100; a++)
+	{
+		if ((a % 3 == 0) && (a % 5 == 0))
+		{
+			printf(" FizzBuzz");
+		}
+		else if (a % 3 == 0)
+		{
+			printf(" Fizz");
+		}
+		else if (a % 5 == 0)
+		{
+			printf(" Buzz");
+		}
+		else
+		{
+			printf(" %d", a);
+		}
+	}
+	printf("\n");
+	return (0);
+}
