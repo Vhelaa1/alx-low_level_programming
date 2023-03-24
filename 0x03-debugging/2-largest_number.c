211e3e7b07abcbce417778099cede0334197a073
HALA <vhelatique123@gmail.com>
2023-03-23 19:03:09 -0700

    f

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,20 +10,26 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+int largest;
 
-    if (a > b && b > c)
-    {
-        largest = a;
-    }
-    else if (b > a && a > c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
+if (a > b && b > c)
+{
+largest = a;
+}
+else if (a > b && c > b)
+largest = a;
+else if (b > a && a > c)
+{
+largest = b;
+}
+else if (b > c && c > a)
+largest = b;
+else if (c > a && a > b)
+largest = c;
+else
+{
+largest = c;
+}
 
-    return (largest);
+return (largest);
 }
