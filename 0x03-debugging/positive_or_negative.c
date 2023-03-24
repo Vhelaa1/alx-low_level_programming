a66c6841bc7d9c841fbe56e55efb0912003231cd
HALA <vhelatique123@gmail.com>
2023-03-23 18:32:54 -0700

    l

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+void positive_or_negative.c(int i)
+{
+	if (i > 0)
+		printf(*%d is positive\n*, i);
+
+	else if (i < 0)
+		printf(*%d is negative\n*, i);
+
+	else
+		printf(*%d is zero\n*, i);
+}
