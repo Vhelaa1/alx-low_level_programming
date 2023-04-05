ca724e38cdbd42e8ef76565bbf5688bc94941d91
HALA <vhelatique123@gmail.com>
2023-04-05 16:51:55 -0700

    f

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - powerr
+ *
+ * @x: int
+ *
+ * @y: int
+ * Return: int
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y != 0)
+		return (x * _pow_recursion(x, y - 1));
+
+	else
+	{
+		return (1);
+	}
+
+}
