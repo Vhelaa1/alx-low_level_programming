bc5039964b4a393999b639e5d1d64312d9db93fb
HALA <vhelatique123@gmail.com>
2023-04-05 16:31:08 -0700

    f

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ *
+ * @s: string
+ *
+ * Return: 0;
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
