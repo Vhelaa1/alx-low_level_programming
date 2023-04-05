a5174cba4dd2125f61d064d10bf239e2adc07079
HALA <vhelatique123@gmail.com>
2023-04-05 15:45:50 -0700

    g

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+/**
+ * _putchar - writes c to stdout
+ *
+ * @c: the character to print
+ *
+ * Return: on success 1
+ * On error: =1 is returned, and errno is set appropriately
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
