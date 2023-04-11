ebdfa5a0a3417faff1f015d31604f2ee93697a05
vhelaa1 <halabouzidi@icloud.com>
2023-04-11 23:21:18 +0000

    array with malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 	char *new_array = malloc(size * sizeof(char));
 	unsigned int i = 0;
 
-	if (size == 0 )
+	if (size == 0)
 		return (NULL);
 	if (new_array == NULL)
 		return (NULL);
