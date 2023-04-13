d46523dd5f77ef5700b917935d3b5d03cdcfbd2c
vhelaa1 <halabouzidi@icloud.com>
2023-04-13 22:52:07 +0200

    fixing betty

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,9 +4,9 @@
 /**
  * malloc_checked - allocates memory using malloc.
  * @b: size of allocated memory
+ *
  * Return: pointer to the allocated memory.
  */
-
 void *malloc_checked(unsigned int b)
 {
 	void *i;
