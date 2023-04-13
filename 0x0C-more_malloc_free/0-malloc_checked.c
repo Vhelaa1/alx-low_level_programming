10a8449942df86e238e69e05df6e89f31cc21341
vhelaa1 <halabouzidi@icloud.com>
2023-04-13 22:46:17 +0200

    fixing betty

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,18 +1,23 @@
+table File  20 lines (17 sloc)  280 Bytes
+ 
+
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * malloc_checked - cause normal process termination with a status value of 98
- * @b: allocated memory
- * Return: a pointer to the allocated memory
+ * malloc_checked -allocates memory using malloc.
+ * @b: allocated memo
+ * Return: pointer to the allocated memory.
  */
+
 void *malloc_checked(unsigned int b)
 {
-char *i;
+	char *i;
 
-i = malloc(b);
-if (i == NULL)
-{
-exit(98);
-}
-return (i);
+	i = malloc(b);
+	if (i == NULL)
+	{
+		exit(98);
+	}
+	return (i);
 }
