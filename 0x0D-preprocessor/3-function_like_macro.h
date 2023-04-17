4d258630e4af249c45e63b94fbba5bbf28815471
Vhelaa1 <halabouzidi@icloud.com>
2023-04-17 21:29:31 +0200

    function-like macro SUM(x, y) that computes the sum of the numbers x and y

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef _3_FUNCTION_LIKE_MACRO_H_
 #define _3_FUNCTION_LIKE_MACRO_H_
 
-#define ABS(a) ((a) < 0 ? (-(a)) : (a))
+#define ABS(x) ((x) < 0 ? (-(x)) : (x))
 
 #endif /* _3_FUNCTION_LIKE_MACRO_H_ */
