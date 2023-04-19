e08163f70a5b86b0802f7ba45761ad5e368bd49b
HALA DRISSI <halabouzidi@icloud.com>
2023-04-19 21:50:58 +0200

    a program that performs simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * main - ...
+  * @argc: ...
+  * @argv: ...
+  *
+  * Return: ...
+  */
+int main(int argc, char *argv[])
+{
+	int (*opr)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	opr = get_op_func(argv[2]);
+
+	if (!opr)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", opr(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
