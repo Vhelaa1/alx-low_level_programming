4d6986988a69132b0faedbb6f653fe9a820496b9
HALA DRISSI <halabouzidi@icloud.com>
2023-04-20 16:58:54 +0200

    function that prints anything

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,32 +1,38 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * print_sings - returns strings passed to the funct separed by separators
+ * print_strings - returns strings passed to the funct separed by separators
  * @separator: A string that seperates strings
  * @n: number of unnamed parameters
  * Return: 0
  */
 
+#include <stdio.h>
+#include <stdarg.h>
+
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	char *s;
-	unsigned int i;
-	va_list argp;
+va_list args;
+unsigned int i;
+char *str;
 
-	va_start(argp, n);
-	if (separator == NULL)
-		separator = "";
+va_start(args, n);
 
-	for (i = 0; i < n; i++)
-	{
-		s = va_arg(argp, char*);
-		if (s == NULL)
-			s = "(nil)";
-		printf("%s", s);
-		if (i < n - 1)
-			printf("%s", separator);
-	}
-	printf("\n");
-	va_end(argp);
+for (i = 0; i < n; i++)
+{
+str = va_arg(args, char *);
+
+if (str == NULL)
+printf("(nil)");
+else
+printf("%s", str);
+
+if (separator != NULL && i < n - 1)
+printf("%s", separator);
+}
+
+printf("\n");
+va_end(args);
 }
