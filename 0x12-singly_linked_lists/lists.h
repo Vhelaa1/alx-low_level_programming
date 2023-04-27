16594a9896abea9836d4bdd7be0eb76ccd874de8
HALA DRISSI <halabouzidi@icloud.com>
2023-04-28 01:42:50 +0200

    a function that frees a list_t list.

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -20,5 +20,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 unsigned int _strlen(const char *s);
 list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif
