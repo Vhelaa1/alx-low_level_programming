635ee7e077327207a844ec9e01730beeced27287
HALA DRISSI <halabouzidi@icloud.com>
2023-05-01 16:37:02 +0200

    prints a listint_t linked list.

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -26,6 +26,8 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
+const listint_t **_r(const listint_t **list, size_t size, const listint_t *new);
+size_t print_listint_safe(const listint_t *head);
 
 
 #endif /*LISTS_H*/
