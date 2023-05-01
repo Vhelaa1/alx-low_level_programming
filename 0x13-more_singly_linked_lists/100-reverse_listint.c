c0f4e3024127e51ffc8dfb7f3350145a42bf34c7
HALA DRISSI <halabouzidi@icloud.com>
2023-05-01 16:32:23 +0200

    fixed

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,7 +8,7 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *current_node, *prev_node, *next_node;
+	listint_t *prev_node, *next_node;
 
 	if (head == NULL || *head == NULL)
 		return (NULL);
