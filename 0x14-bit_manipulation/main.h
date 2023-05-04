b2a4d82a653e89bc501d526b9af17b7f3bbac577
HALA DRISSI <halabouzidi@icloud.com>
2023-05-04 23:27:04 +0200

    sets the value of a bit to 1 at a given index.

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,6 +4,7 @@ unsigned int binary_to_uint(const char *b);
 unsigned long int _power(unsigned int base, unsigned int pow);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 int _putchar(char c);
 
 #endif
