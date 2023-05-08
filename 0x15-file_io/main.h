dc599e351fdf53e857d12a81d1dbe59c09b5f724
HALA DRISSI <halabouzidi@icloud.com>
2023-05-08 22:54:39 +0200

    displays the information in ELF header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,6 +10,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 size_t _strlen(char *str);
 int append_text_to_file(const char *filename, char *text_content);
-
+int _putchar(char c);
 
 #endif
